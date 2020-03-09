fe778bcb5ee16060b256722648968dd96b4386d1
Argha-m <mallickargha1@gmail.com>
2020-03-09 13:07:25 +0530

    Updates

---
+++ precache-manifest.00fb4ddd112f6471577c56ff30be1417.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe1233ad26f32098269ecb5eb735dab",
+    "url": "/demo-project/index.html"
+  },
+  {
+    "revision": "f0416e891cc65b8be7cd",
+    "url": "/demo-project/static/css/2.765f46be.chunk.css"
+  },
+  {
+    "revision": "ef538937960fa73cda2a",
+    "url": "/demo-project/static/css/main.7abcb3b7.chunk.css"
+  },
+  {
+    "revision": "f0416e891cc65b8be7cd",
+    "url": "/demo-project/static/js/2.d41d4e53.chunk.js"
+  },
+  {
+    "revision": "737c22aa17b7146f6f3e7bdc06ec8454",
+    "url": "/demo-project/static/js/2.d41d4e53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef538937960fa73cda2a",
+    "url": "/demo-project/static/js/main.14bb80e3.chunk.js"
+  },
+  {
+    "revision": "fca134b5ac7cb0758ce2",
+    "url": "/demo-project/static/js/runtime-main.80316e39.js"
+  },
+  {
+    "revision": "2f8dd40788b1c1813890aa9c9a2a1d13",
+    "url": "/demo-project/static/media/banner-bg.2f8dd407.jpg"
+  },
+  {
+    "revision": "5e63086c09005d7a032103e98bfa44e1",
+    "url": "/demo-project/static/media/testimonial-2.5e63086c.jpg"
+  }
+]);
