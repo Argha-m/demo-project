75b36d1cf3dd8aefe7023f14e1b13dba4b778c05
Argha-m <mallickargha1@gamil.com>
2020-03-09 11:25:39 +0530

    commit for about page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -54,7 +54,7 @@ export class About extends React.Component{
                                             <h4>About Me</h4>
                                             <p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla porttitor accumsan tincidunt.</p>
 
-                                            <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. porttitor at sem.</p>
+                                            <p>Mauris aliquet elit, eget tincidunt nibh pulvinar a Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. porttitor at sem.</p>
                                             
                                             <p>Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Nulla porttitor accumsan tincidunt. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
                                         </div>
